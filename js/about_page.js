4472fb4ab9ed77a11779605023bec432f17d9edd
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-24 17:45:39 -0500

    add about_page.js file and code

---
+++ js/about_page.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $('#familyPicture').append(`
+    <img src="images/40325511_2151190714892961_7091197602212872192_n.jpeg" alt="Me with my son, Tyler" class="familyPic">
+    `)
+
+    $('#formalPicture').append(`
+    <img src="images/image_6483441-1.JPG" alt='Me in a tie, yuck' class="formalPic">
+    `)
+
+    $('#armyPicture').append(`
+    <img src="images/image_6483441.JPG" alt="In the Army Now" class="armyPic">
+    `)
+
+    $('#aboutMeTextArea').append(`
+    <p class="merriweather text-center mt-5">Hello! This is me. More to follow.</p>
+    `)
+})
