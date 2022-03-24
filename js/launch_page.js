4e555a59169a12799631e0ed56a6d646a2a1db7f
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-24 17:46:42 -0500

    change styling, add more functionality to nav button area

--- js/launch_page.js
+++ js/launch_page.js
@@ -1,14 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     let launchNameAndTitle = $('#launchNameAndTitle');
+    let launchButtonArea = $('#launchButtonArea');
+    let about = $('#aboutArea');
 
     //Appends my name to the section with id of launchName
     launchNameAndTitle.append(`
-    <h1 id='myName' class="launchItems gradient-text">James R. Woodward</h1>
+    <h1 id='myName' class="launchItems biggerH1 merriweather">James R. Woodward</h1>
+    <h2 id='jobTitle' class="launchItems biggerH2 merriweather">Full Stack<br>Web Developer</h2>
     `)
 
-    //Appends title to section with id of launchName
-    launchNameAndTitle.append(`
-    <h2 id='jobTitle' class="launchItems gradient-text">Full Stack<br>Web Developer</h2>
+    launchButtonArea.append(`
+    <div id="buttonArea">
+        <div id="launchButton" class="d-flex align-items-center justify-content-center">
+        <p class="merriweather my-auto">Launch</p>
+        </div>
+        <div id="launchButtonClicked" class="justify-content-center row">
+            <a href="#aboutArea" class="d-flex col-4 text-center justify-content-center text-decoration-none"><p class="merriweather navItemsLaunch align-self-center" id="aboutTopLink">About</p></a>
+            <a class="d-flex col-4 text-center justify-content-center text-decoration-none"><p class="merriweather navItemsLaunch align-self-center" id="skillsTopLink">Skills</p></a>
+            <a class="d-flex col-4 text-center justify-content-center text-decoration-none"><p class="merriweather navItemsLaunch align-self-center" id="projectsTopLink">Projects</p></a>
+            <a class="d-flex col-6 text-center justify-content-center text-decoration-none"><p class="merriweather navItemsLaunch align-self-center" id="resumeTopLink">Resume</p></a>
+            <a class="d-flex col-6 text-center justify-content-center text-decoration-none"><p class="merriweather navItemsLaunch align-self-center" id="contactTopLink">Contact</p></a>
+        </div>
+    </div>
     `)
+
+    $('#launchButton').click(function (e) {
+        e.preventDefault();
+            $('#launchButton').addClass('ld ld-vortex-out')
+        setTimeout(function(){
+            $('#launchButton').addClass('d-none')
+        }, 1050)
+        setTimeout( function(){
+            $('#launchButtonClicked').css('display', 'block')
+            $('#launchButtonClicked').addClass('ld ld-float-btt-in')
+        }, 1100)
+        setTimeout(function(){
+            $('#launchButtonClicked').addClass('d-flex')
+            setTimeout(function(){
+                // $("p").fadeIn(2000);
+                $("p").css("display", 'block');
+                $("p").addClass('ld ld-flip-v-in');
+            },100)
+        }, 1500)
+    })
 })
+
